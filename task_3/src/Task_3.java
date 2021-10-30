d2dc50ca910bb1fe0ce7c1e3067e36077f74091c
fudzich <vsevolod656@gmail.com>
2021-10-30 15:37:16 +0300

    task_3_1

--- task_3/src/Task_3.java
+++ task_3/src/Task_3.java
@@ -1,13 +1,26 @@
 public class Task_3 implements Task_3_base {
     @Override
     public int subtask_1_for(int n1, int n2, int a, int b) {
+        int counter = 0;
+        for(int i = n1; i <= n2; i++)
+        {
+            if((i % a == 0) && ( i % b !=0))
+            {
+                counter++;
+            }
+        }
+        return counter;
         // подсчитать, сколько чисел, кратных a, но не кратных b,
         // находится между числами n1 и n2 включительно
-        return 0;
+        //return 0;
     }
 
     @Override
     public int subtask_2_for(int num) {
+        for(int i = 1; i <= num; i++)
+        {
+            double posled = i + i * Math.pow(10,i-1);
+        }
         // Последовательность чисел строится следующим образом:
         // сначала идет одна единица,
         // потом две двойки,
@@ -22,19 +35,36 @@ public class Task_3 implements Task_3_base {
 
     @Override
     public int subtask_3_for(int num, int d, int cnt) {
+        int a1 = num;
+        int sum = a1;
+        for( int i = 1; i < cnt; i++)
+        {
+            int a2 = a1 * d + 1;
+            a1 = a2;
+            sum = sum + a2;
+        }
+        return sum;
         // Дана последовательность
         // a(0) = num
         // a(n) = a(n - 1) * d + 1
         // Найти сумму первых cnt элементов последовательности
-        return 0;
+        //return 0;
     }
 
     @Override
     public int subtask_4_for(int n) {
+        int s = 1;
+        int factorial = 1;
+        for (int i = 2; i <= n; i++)
+        {
+            factorial = factorial * i;
+            s = s + factorial;
+        }
+        return s;
         // Вычислить сумму
         // S(n) = 1 + 1 * 2 + 1 * 2 * 3 + ... + n!
         // для заданного n
         // (n! - это n-факториал. Кто не знает - гуглите)
-        return 0;
+        //return 0;
     }
 }
