6caa605db885cfd687612e67173397ab77851b87
fudzich <vsevolod656@gmail.com>
2022-01-10 21:54:09 +0300

    test complete

--- task_10/src/core/Payment.java
+++ task_10/src/core/Payment.java
@@ -3,12 +3,30 @@ package core;
 public class Payment {
     private boolean type;
     // type true = "Платежное поручение", type false = "Банковский ордер"
+    private int total;
+    private int number;
+    private String dogovorNumber;
+    private String date;
+    private int ammount;
 
     public Payment(boolean type){
         this.type = type;
     }
 
+    public Payment(int total, int number, boolean type, String dogovorNumber, String date){
+        this.total= total;
+        this.number = number;
+        this.type = type;
+        this.dogovorNumber = dogovorNumber;
+        this.date = date;
+        ammount = 1;
+    }
+
     public boolean GetType(){
         return type;
+    }
+
+    public int GetAmmount(){
+        return ammount;
     }
 }
