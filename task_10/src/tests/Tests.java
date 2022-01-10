12059b46aa9848231e39130ec6d0422f1ec3698c
fudzich <vsevolod656@gmail.com>
2022-01-10 20:28:44 +0300

    created test: Dogovor_AddNewDogovor_DogovorAmmountEquelsOne

--- task_10/src/tests/Tests.java
+++ task_10/src/tests/Tests.java
@@ -1,6 +1,13 @@
 package tests;
 
 import org.junit.*;
+import  core.*;
 
 public class Tests extends Assert {
+
+    public void Dogovor_AddNewDogovor_DogovorAmmountEquelsOne(){
+        Dogovor dogovor = new Dogovor();
+        dogovor.addNewDogovor("1","20211210");
+        assertEquals(1, dogovor.getDogovorAmmount());
+    }
 }
