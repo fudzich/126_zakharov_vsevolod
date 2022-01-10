7403208de9cfa18ae43b57fda050e6bc88cd1aa7
fudzich <vsevolod656@gmail.com>
2022-01-10 21:01:04 +0300

    complete test

--- task_10/src/core/Dogovor.java
+++ task_10/src/core/Dogovor.java
@@ -1,5 +1,20 @@
 package core;
 
+import java.util.HashMap;
+
 public class Dogovor {
 
+    private String number;
+    private String date;
+    private int ammount;
+
+    public Dogovor(String number, String date){
+        this.number = number;
+        this.date = date;
+        ammount=1;
+    }
+
+    public int getDogovorAmmount(){
+        return ammount;
+    }
 }
