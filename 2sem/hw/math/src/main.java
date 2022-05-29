0b5edfeaab375718cc1cce3b2f7f55bd2896c0b9
fudzich <vsevolod656@gmail.com>
2022-05-29 16:44:57 +0300

    tsk_5

---
+++ 2sem/hw/math/src/main.java
@@ -0,0 +1,12 @@
+public class main {
+
+    int main(){
+        int k=0;
+        for(int i=1000;i<=9999;i++){
+            if(i%29==0){
+                k++;
+            }
+        }
+        return k;
+    }
+}
